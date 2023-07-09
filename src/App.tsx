5b589badef73dd2734094265343e4e0ed74daf5a
GabrielHV9 <gdhernandezv@correo.udistrital.edu.co>
2023-07-09 13:18:27 -0500

    ruta de salas

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LogInView } from "./views/Auth/LogIn";
 import { ReactElement } from "react";
 import { RegisterView } from "./views/Auth/Register";
 import { ResetPasswordView } from './views/Auth/ResetPassword/index';
-import { SillasView } from "./views/cinema/Sillas";
+import { SillasView } from "./views/Auth/cinema/Sillas";
 import { RoutesPathsEnum } from "./helpers/RoutePaths";
 import { Toaster } from "react-hot-toast";
 import { Home } from "./views/Home";
@@ -21,7 +21,7 @@ const App: React.FC = (): ReactElement => {
             />
           <Route path={RoutesPathsEnum.HOME}
 
-            element={<RegisterView/>} //CAMBIAR RUTA DEPENDIENDO DEL MODULO QUE SE DESEE VER 
+            element={<Home/>} //CAMBIAR RUTA DEPENDIENDO DEL MODULO QUE SE DESEE VER 
 
           />
           <Route path={RoutesPathsEnum.LOGIN}
@@ -30,6 +30,9 @@ const App: React.FC = (): ReactElement => {
           <Route
             path={RoutesPathsEnum.RESETPASSWORD}
             element={<ResetPasswordView/>}
+          />
+          <Route path={RoutesPathsEnum.CINEMA}
+            element={<SillasView/>}
           />
           <Route path="*" element={<Navigate replace to="/auth/login" />} />
         </Routes>
