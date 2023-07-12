612a54e3fae3c172babf462e2824bfd48fdc35f8
Oscar <omcarrillog@udistrital.edu.co>
2023-07-11 22:48:11 -0500

    Agregados algunos estilos básicos a el componente Pay
    Se implemento la suma de puntos solamente con el valor de los puntos que suma por la compra de boletos.

--- src/components/Pay/index.jsx
+++ src/components/Pay/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { MainLayout } from '../../layouts/MainLayout';
+import './index.css';
 
 export const Pay = () => {
   const [numeroTarjeta, setNumeroTarjeta] = useState('');
@@ -8,7 +9,7 @@ export const Pay = () => {
   const [mensaje, setMensaje] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
-
+  const puntosPelicula = 10;
 
   const handleChangeNumeroTarjeta = (e) => {
     setNumeroTarjeta(e.target.value);
@@ -30,52 +31,58 @@ export const Pay = () => {
     const esTarjetaValida = validarTarjeta(numeroTarjeta);
 
     if (esTarjetaValida) {
-      setMensaje(`Transacción exitosa. Usted ha sumado X puntos.`);
+      setMensaje(`Transacción exitosa. Con esta compra has sumado ${puntosPelicula*location.state.listPost.length} puntos!`);
 
-      console.log( "Este es el location state desde Pay")
-      console.log(location.state)
-      // Aquí puedes realizar las acciones necesarias para sumar los puntos
+      console.log("Este es el location state desde Pay");
+      console.log(location.state);
+      // Acciones necesarias para sumar los puntos...
+      
       setTimeout(() => {
         navigate('/home', { state: null });
-      }, 3000);
-      
+      }, 5000);
     } else {
       setMensaje('Número de tarjeta no válido');
     }
   };
 
   const validarTarjeta = (numeroTarjeta) => {
-    // Aquí puedes implementar tu lógica de validación de tarjeta
-    // Puedes utilizar bibliotecas externas o algoritmos de validación
-
-    // Ejemplo básico de validación (solo para demostración)
+    // Implementar lógica de validación de tarjeta
     return numeroTarjeta.length === 16;
   };
 
+  const isTarjetaValida = validarTarjeta(numeroTarjeta);
+  const inputClassName = isTarjetaValida ? 'transaction-input valid' : 'transaction-input invalid';
+  const messageClassName = mensaje === 'Número de tarjeta no válido' ? 'transaction-message invalid' : 'transaction-message valid';
+
   return (
-    <>
-      <MainLayout>
-        <div>
-          <div>
-            Total a pagar: {location.state.totalCheckout}$
-          </div>
-          <h1>Validación de Tarjeta de Pago</h1>
-          <input
-            type="text"
-            value={numeroTarjeta}
-            onChange={handleChangeNumeroTarjeta}
-            placeholder="Ingrese el número de tarjeta Visa o MasterCard"
-          />
-          <button onClick={handleValidarTarjeta}>Validar</button>
-          {mensaje && <p>{mensaje}</p>}
-          {tipoTarjeta && <p>Tipo de tarjeta: {tipoTarjeta}</p>}
+    <MainLayout>
+      <div className="transaction-container">
+        <div className="transaction-title">TRANSACCIÓN</div>
+        <div className="transaction-total">
+          Total a pagar: {location.state.totalCheckout}$
         </div>
-      </MainLayout>
-    </>
+        <h1 className="transaction-title">Validación de Tarjeta de Pago</h1>
+        <input
+          className={inputClassName}
+          type="text"
+          value={numeroTarjeta}
+          onChange={handleChangeNumeroTarjeta}
+          placeholder="Ingrese el número de tarjeta Visa o MasterCard"
+        />
+        <button className="transaction-button" onClick={handleValidarTarjeta}>
+          Validar
+        </button>
+        <p className={messageClassName}>{mensaje}</p>
+        {tipoTarjeta && (
+          <p className={`transaction-type ${tipoTarjeta.toLowerCase()}`}>
+            Tipo de tarjeta: {tipoTarjeta}
+          </p>
+        )}
+      </div>
+    </MainLayout>
   );
 };
 
 export default Pay;
 
 
-//export default Pay
